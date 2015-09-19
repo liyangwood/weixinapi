c8bb4695d734741f01d4058d3c409b57fc4908c2
Jacky <liyang@chinagate.com>
2015-09-19 08:58:35 -0700

    update

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,27 @@ var router = express.Router();
 var apiFn = require('../weixin/fn');
 
 
-router.get('/', function(req, res, next){
-    res.render('admin/index', {
-        title : '微信公众号管理后台－－首页'
-    });
+router.get('/:first', function(req, res, next){
+    var first = req.params.first || 'welcome';
+
+
+    F.renderPath(first, req, res);
 });
+
+router.get('/', function(req, res, next){
+    F.renderPath('welcome', req, res);
+});
+
+
+
+
+var F = {
+   renderPath : function(file, req, res){
+       res.render('admin/index', {
+           title : '微信公众号管理后台－－首页',
+           filePath : 'admin/'+file
+       });
+   }
+};
 
 module.exports = router;
