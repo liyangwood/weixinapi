4aaf8c36d625f8445e249a36088aff545f81e0ea
Jacky <liyang@chinagate.com>
2015-09-21 15:02:59 -0700

    update

--- routes/weixinapi.js
+++ routes/weixinapi.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var apiFn = require('../weixin/fn');
 
+var request = require('request');
+
 
 router.get('/js_ticket', function(req, res, next){
     apiFn.getJsConfig({}, function(rs){
@@ -84,15 +86,39 @@ router.get('/massSendText', function(req, res, next){
     var text = '<a href="http://www.wenxuecity.com/news/2015/09/21/4580049.html">令完成筹码是什么？曝手握中共海外间谍绝密名单(图)</a> \n\n'
         +'<a href="http://www.wenxuecity.com/news/2015/09/21/4580011.html">西雅图加紧准备迎接习近平到访 将暂关9个街区(图)</a>\n\n'
         +'<a href="http://www.wenxuecity.com/news/2015/09/21/4579964.html">30年过去了 美方翻出习近平首次访美的名片(组图)</a>';
-    apiFn.massSendText(text, function(err, rs){
-        if(err){
-            res.json(err);
+    text = '文学城最新新闻列表，查看更多请点击底部菜单。\n\n';
+
+    var max = 12;
+
+    request('http://api.wenxuecity.com/service/api/?act=index&channel=news&pagesize=10&version=2&format=json', function(err, response, body){
+        //res.json(JSON.parse(body).list);
+        var list = JSON.parse(body)['list'];
+        if(!list){
+            res.json(error);
             return;
         }
 
-        res.json(rs);
+        for(var i= 0, len=(list.length>max?max:list.length); i<len; i++){
+            var d = list[i].dateline.substring(0, 10).replace(/\-/g, '/');
+            text += (i+1)+'. <a href="http://www.wenxuecity.com/news/'+d+'/'+list[i].postid+'.html">'+list[i].title+'</a> \n\n';
+        }
+
+        console.log(text);
+
+        apiFn.massSendText(text, function(err, rs){
+            if(err){
+                res.json(err);
+                return;
+            }
+
+            res.json(rs);
+
+        });
 
     });
+
+
+
 });
 
 router.get('/massSendNews', function(req, res, next){
