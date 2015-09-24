4a75427b86736c74a48ddc7d44be3901db4a3403
Jacky <liyang@chinagate.com>
2015-09-24 14:40:03 -0700

    update

--- app.js
+++ app.js
@@ -17,17 +17,7 @@ var weixinapi = require('./routes/weixinapi');
 var adminApi = require('./routes/admin');
 
 
-var wechatList = require('wechat').List;
-wechatList.add('union_start_session', [
-    ['回复{a}查看我的性别', function (info, req, res) {
-        console.log(info, req, res);
-        res.reply('我是个妹纸哟');
-    }],
-    ['回复{b}查看我的年龄', function (info, req, res) {
-        res.reply('我今年18岁');
-    }],
-    ['回复{c}查看我的性取向', '这样的事情怎么好意思告诉你啦- -']
-]);
+
 
 
 var app = express();
@@ -53,6 +43,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(express.query());
+
+var wechatList = require('wechat').List;
+wechatList.add('union_start_session', [
+    ['回复{a}查看我的性别', function (info, req, res) {
+        console.log(res.reply);
+        res.reply('我是个妹纸哟');
+    }],
+    ['回复{b}查看我的年龄', function (info, req, res) {
+        res.reply('我今年18岁');
+    }],
+    ['回复{c}查看我的性取向', '这样的事情怎么好意思告诉你啦- -']
+]);
+
+
 app.use('/fremontapi', wechat({
     token: weixinConfig.WEIXIN_CONFIG.token,
     appid: weixinConfig.WEIXIN_CONFIG.appID,
