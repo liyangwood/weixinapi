3009377bb833adfa6dc78b60dc3036ca8c8b1c1a
Jacky <liyang@chinagate.com>
2015-10-03 12:27:09 -0700

    update

--- public/func/publish.js
+++ public/func/publish.js
@@ -10,6 +10,7 @@
     })();
 
     var previewBox, titleBox, addText, addTextBtn, pubBtn, addImage;
+    var addImageBtn;
 
     var F = {
         init : function(){
@@ -17,6 +18,11 @@
             F.initView();
             F.initEvent();
             F.initEnd();
+
+            if(isInWeixin){
+                F.initWeixinConfig();
+            }
+
         },
         initVar : function(){
             previewBox = $('#js_preview');
@@ -24,6 +30,7 @@
             addText = $('.js_addText');
             addTextBtn = $('.js_addBtn');
             addImage = $('.js_addImage');
+            addImageBtn = $('.js_addImageBtn');
         },
         initView : function(){
 
@@ -53,23 +60,68 @@
                 addImage.change(function(){
                     var file = addImage[0].files[0];
                     console.log(file);
-                    F.previewImageByFile(file);
+                    F.addPreviewImage('file', file);
                     addImage.val('');
+                });
+
+                addImageBtn.click(function(e){
+                   addImage.trigger('click');
+                });
+            }
+            else{
+                addImageBtn.click(function(){
+                    wx.chooseImage({
+                        count: 2, // 默认9
+                        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                        success: function (res) {
+                            var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                            F.addPreviewImage('weixin', localIds);
+                        }
+                    });
                 });
             }
         },
 
-        previewImageByFile : function(file){
+        addPreviewImage : function(type, data){
             var img = document.createElement('img');
-            img.file = file;
 
-            var fr = new FileReader();
-            fr.onload = function(e){
-                img.src = e.target.result;
-            };
-            fr.readAsDataURL(file);
+            if(type === 'file'){
+                img.file = data;
+
+                var fr = new FileReader();
+                fr.onload = function(e){
+                    img.src = e.target.result;
+                };
+                fr.readAsDataURL(data);
+            }
+            else if(type === 'weixin'){
+                img.src = data;
+            }
+
 
             previewBox.find('section').append(img);
+        },
+
+        initWeixinConfig : function(){
+            var url = '/wxapi/js_ticket?url='+encodeURIComponent(location.href.replace(location.hash, ''));
+            $.ajax({
+                url : url,
+                type : 'GET',
+                dataType : 'json',
+                success : function(config){
+                    wx.config({
+                        debug : true,
+                        appId : config.appId,
+                        timestamp : config.timestamp,
+                        nonceStr : config.nonceStr,
+                        signature : config.signature,
+                        jsApiList : config.jsApiList
+                    });
+                }
+            });
+
+
         }
     };
 
